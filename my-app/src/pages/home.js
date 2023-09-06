1830f836a8c288d7cc8a1116a5fe44c0914b483c
visathongdee <tthongde@purdue.edu>
2023-09-06 17:49:41 -0400

    Added side lines for email and socials

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "../style.css";
-import { FloatButton, Anchor } from "antd";
+import { FloatButton, Anchor, Affix } from "antd";
 import Landing from "./sections/landing";
 import AboutMe from "./sections/aboutMe";
 import Experience from "./sections/experience.tsx";
 import Projects from "./sections/projects.tsx";
 import Contact from "./sections/contact.tsx";
+import { Github, Linkedin, Mail } from "lucide-react";
 
 export default function Home() {
 
   const items = [
+    // {
+    //   key: "#",
+    //   href: "#",
+    //   title: "l",
+    // },
     {
       key: "aboutMe",
       href: "#aboutMe",
@@ -34,9 +40,25 @@ export default function Home() {
 
   return (
     <div>
-      <FloatButton.BackTop className="floatbutton"/>
-      <Anchor direction="horizontal" items={items} className="anchor" />
+      <FloatButton.BackTop className="floatButton" style={{color: "red"}}/>
+        <Anchor direction="horizontal" affix="true" items={items} />
+      <Affix offsetTop={150}>
+        <div className="topSideline">
+          <hr className="rightSidelineLine" />
+          <a href="mailto:visathongdee@gmail.com">
+            <Mail className="sideline-icon" />
+          </a>
+          <a href="https://github.com/visathongdee">
+            <Github className="sideline-icon" />
+          </a>
+          <a href="https://linkedin.com/in/visathongdee">
+            <Linkedin className="sideline-icon" />
+          </a>
+        </div>
+      </Affix>
+      <div>
       <Landing />
+      </div>
       <div id="aboutMe">
         <AboutMe />
       </div>
@@ -49,6 +71,14 @@ export default function Home() {
       <div id="contact">
         <Contact />
       </div>
+      <Affix offsetBottom={200}>
+        <div className="bottomSideline">
+          <hr className="leftSidelineLine" />
+          <p>
+            visathongdee@gmail.com
+          </p>
+        </div>
+      </Affix>
     </div>
   );
 }
